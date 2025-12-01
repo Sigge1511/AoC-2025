ec777a63d6ad6ae4bdd85ca6e6775fc911d44679
Maja Sigfeldt <msigfeldt@gmail.com>
2025-12-02 00:55:49 +0100

    in progress with attempt design 1

--- Program.cs
+++ Program.cs
@@ -1,19 +1,31 @@
 ﻿
 public class AoC2025 
-{  
+{
     public static void Main()
     {
+        int pointingto = 50;
+        int turnSteps = 0;
+        int zeronumber = 0;
+
+        var wheelTurner = new AoC_2025.TurnTheWheel();
+
+        string turnInstruction = "L5";
+        string direction = turnInstruction[0].ToString();
+
+        switch (direction)
+        {
+            case "L":
+                (pointingto, zeronumber) = wheelTurner.SpinL(pointingto, turnSteps, zeronumber);
+                break;
+            case "R":
+                (pointingto, zeronumber) = wheelTurner.SpinR(pointingto, turnSteps, zeronumber);
+                break;
+            default:
+                Console.WriteLine("Invalid direction");
+                break;
+        }
 
 
-
-
-
-
-
-
-
-
-
-
+        Console.WriteLine($"The new password is: {zeronumber}, since the value 0 appeared {zeronumber} times. \nGood luck!");
     }
 }
---
+++ TurnTheWheel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2025
+{
+    internal class TurnTheWheel
+    {
+        //Metod returnerar en tuple två ints, där första int är den nya positionen på hjulet
+        //efter att ha snurrat, andra är det uppdaterade antalet gånger hjulet har pekat på noll.
+        public (int, int) SpinL(int pointingto, int turnSteps, int zeronumber)
+        {
+            foreach (var turns in Enumerable.Range(0, turnSteps))
+            {
+                pointingto--;
+                if (pointingto == 100)
+                {
+                    pointingto = 0;
+                }
+                else if (pointingto == -1)
+                {
+                    pointingto = 99;
+                }
+                if (pointingto == 0)
+                {
+                    zeronumber = AddToZeroNumber(zeronumber);
+                }
+
+            }
+            return (pointingto, zeronumber);
+        }
+
+        //Metod returnerar en tuple två ints, där första int är den nya positionen på hjulet
+        //efter att ha snurrat, andra är det uppdaterade antalet gånger hjulet har pekat på noll.
+        public (int, int) SpinR(int pointingto, int turnSteps, int zeronumber)
+        {
+            foreach (var turns in Enumerable.Range(0, turnSteps))
+            {
+                pointingto++;
+                if (pointingto == 100)
+                {
+                    pointingto = 0;
+                }
+                else if (pointingto == -1)
+                {
+                    pointingto = 99;
+                }
+                if (pointingto == 0)
+                {
+                    zeronumber = AddToZeroNumber(zeronumber);
+                }
+
+            }
+            return (pointingto, zeronumber);
+        }
+
+        public int AddToZeroNumber(int zeronumber)
+        {
+            return zeronumber++;
+        }
+
+
+
+    }
+}
